09369820bf6573bed14e9f692f533a8adb4e6f39
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-13 15:24:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,30 +8,59 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300", alt:"Image 3"},
 ];
 
-btn.addEventListener('click', () => {downloadImages(images);});
+// btn.addEventListener('click', () => {downloadImages(images);});
 
 function downloadImages(images){
-	const promises = images.map(image => {
-		return new Promise((res, rej) => {
-			const img = new Image();
-			img.src = image.url;
-			img.alt = image.alt;
-			image.onload = () => {
-				res(img);
-			};
-			img.onerror = () => {
-				reject(`Failed to load image's URL: ${image.url}`);
-			};
-		});
-	});
+	// const promises = images.map(image => {
+	// 	return new Promise((res, rej) => {
+	// 		const img = new Image();
+	// 		img.src = image.url;
+	// 		img.alt = image.alt;
+	// 		image.onload = () => {
+	// 			res(img);
+	// 		};
+	// 		img.onerror = () => {
+	// 			reject(`Failed to load image's URL: ${image.url}`);
+	// 		};
+	// 	});
+	// });
 
-	Promise.all(promises)
-	.then(imgs => {
+	// Promise.all(promises)
+	// .then(imgs => {
+	// 	output.innerHTML = null;
+	// 	imgs.forEach(img => {
+	// 		output.appendChild(img);
+	// 	});
+	// }).catch(error => {
+	// 	console.error(error);
+	// });
+	const promises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.alt = image.alt;
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+    });
+  });
+
+  Promise.all(promises)
+    .then(imgs => {
+      const output = document.getElementById('output');
 		output.innerHTML = null;
-		imgs.forEach(img => {
-			output.appendChild(img);
-		});
-	}).catch(error => {
-		console.error(error);
-	});
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
+
+btn.addEventListener('click', () => {
+	downloadImages(images);
+});
