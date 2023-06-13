e3f1b0398c2c5fa021400cf6f4170e1e6384f5a2
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-13 15:16:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,35 @@ const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  { url: "https://picsum.photos/id/237/200/300", alt:"Image 1"},
+  { url: "https://picsum.photos/id/238/200/300", alt:"Image 2"},
+  { url: "https://picsum.photos/id/239/200/300", alt:"Image 3"},
 ];
+
+btn.addEventListener('click', () => {downloadImages(images);});
+
+function downloadImages(images){
+	const promises = images.map(image => {
+		return new Promise((res, rej) => {
+			const img = new Image();
+			img.src = image.url;
+			img.alt = image.alt;
+			image.onload = () => {
+				res(img);
+			};
+			img.onerror = () => {
+				reject(`Failed to load image's URL: ${image.url}`);
+			};
+		});
+	});
+
+	Promise.all(promises)
+	.then(imgs => {
+		output.innerHTML = null;
+		imgs.forEach(img => {
+			output.appendChild(img);
+		});
+	}).catch(error => {
+		console.error(error);
+	});
+}
